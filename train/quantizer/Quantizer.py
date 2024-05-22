if the bitwidth is more than 2, then everything's fine, so we need to fix the scaling issue

train/quantizer/Quantizer.py
@@ -394,7 +394,9 @@ class Quantizer(object):
         
         # quantize activations
         for i in range(num_quant_acts):
-            model = self.quantize_act(model, quantizable_idx[i], int(strategy[i]))
+            model, modified_bit_width = self.quantize_act(model, quantizable_idx[i], int(strategy[i]))
+            if modified_bit_width:
+                strategy[i] += 1
 
         # quantize add outputs and handle residuals
         self.quantize_output(model)
@@ -410,7 +412,7 @@ class Quantizer(object):
         model.train(training_state)
         config.IGNORE_MISSING_KEYS = ignore_missing_keys_state
 
-        return model
+        return model, strategy
 
     def update_index(self, model, quantizable_idx):
         idx = 0
@@ -450,15 +452,26 @@ class Quantizer(object):
                      act_bit_width):
         
         layer_map = self.quantize_kwargs['quant_act_map']
+        modified_bit_width = False
 
         for i, node in enumerate(model.graph.nodes):
             if node.op == 'call_module' and i == act_idx:
                 module = get_module(model, node.target)
-                if isinstance(module, tuple(layer_map.keys())):
+                if isinstance(module, tuple(layer_map.keys())):       
+                    # if the target is convolution next and the bitwidth is 1
+                    # then increase because we need to be able to represent 0
+                    for n in node.users:
+                        if n.op == 'call_module':
+                            m = get_module(model, n.target)
+                            if isinstance(m, nn.Conv2d) or isinstance(m, qnn.QuantConv2d):
+                                if act_bit_width == 1:
+                                    act_bit_width += 1
+                                    modified_bit_width = True
+                    
                     quant_module_class, quant_module_kwargs = layer_map[type(module)]
                     quant_module_kwargs['bit_width'] = act_bit_width
                     quant_module = quant_module_class(**quant_module_kwargs)
-                
+                        
                     if len(node.users) == 1:
                         user_node = list(node.users.keys())[0]
                         if user_node.name.endswith('act_eq_mul'):
@@ -477,7 +490,7 @@ class Quantizer(object):
                     break
             
         model = rewriter.apply(model)
-        return model
+        return model, modified_bit_width
          
     def quantize_output(self,
                         model):